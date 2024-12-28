b21ac5d0b821ac7e24469efb328ab417fc6c38e6
morsalin <mursalim1246@gmail.com>
2024-12-28 06:20:00 +0530

    Initial commit

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div
+      className="h-screen w-screen bg-cover bg-center flex items-center justify-center"
+      style={{ backgroundImage: "url('assets/alien.jpg')" }}
+    >
+      <h1 className="text-4xl font-bold text-white">Site coming soon</h1>
+    </div>
+  );
+}
